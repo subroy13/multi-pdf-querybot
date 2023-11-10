3f4739299695642c50d4c78fdfc829a3c819b553
subhrajyoty <subhrajyoty@syscloudsoftware.com>
2023-11-10 17:05:20 +0530

    fix: bug fixes and multi pdf querybot

--- app/datastore.py
+++ app/datastore.py
@@ -25,31 +25,62 @@ def save_metadata_database(data):
     return True
 
 
-def insert_pdf_to_store(filename: str):
-    loader = PyPDFLoader(TEMP_PDF_FILE_NAME)
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=CHUNK_SIZE, chunk_overlap=0)
-    print('Extracting texts')
-    pages = loader.load_and_split(
-        text_splitter=text_splitter
-    )
-    print('Processing embeddings')
-    chroma_client.add_documents(pages)
-    chroma_client.persist()   # save to the disk
+def insert_pdf_to_store(filename: str, state):
+    fileid = filename.replace('.', '')
+    try:
+        loader = PyPDFLoader(TEMP_PDF_FILE_NAME)
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=CHUNK_SIZE, chunk_overlap=0)
+        state['database']['parse_message'] = '% Extracting texts'
+        pages = loader.load_and_split(
+            text_splitter=text_splitter
+        )
+        docs = []
+        for page in pages:
+            page.metadata['docfilename'] = filename
+            page.metadata['docfileid'] = fileid
+            docs.append(page)
+        state['database']['parse_message'] = '% Extracting embeddings'
+        chroma_client.add_documents(docs)
 
-    print('Saving metadata')
-    metadata = load_metadata_database()
-    metadata[filename.replace('.', '')] = {
-        'fname': filename,
-        'chunks': len(pages),
-        'createdat': dt.datetime.now().strftime('%d-%m-%Y')        
-    }
-    save_metadata_database(metadata)
+        state['database']['parse_message'] = '% Saving to vector store'
+        chroma_client.persist()   # save to the disk
 
+        state['database']['parse_message'] = '% Saving metadata information'
+        metadata = load_metadata_database()
+        metadata[fileid] = {
+            'fname': filename,
+            'chunks': len(pages),
+            'createdat': dt.datetime.now().strftime('%d-%m-%Y %H:%M:%S')        
+        }
+        save_metadata_database(metadata)
+        state['database']['parse_message'] = f"+File {filename} parsed successfully"
+    except Exception as e:
+        state['database']['parse_message'] = f"- {str(e)}\nPlease try again"
+
+def delete_pdf_from_store(fileid: str, state):
+    try:
+        state['database']['parse_message'] = '% Deleting metadata'
+
+        # delete from metadata
+        metadata = load_metadata_database()
+        del metadata[fileid]
+        save_metadata_database(metadata)
+
+        state['database']['parse_message'] = '% Deleting embeddings'
+        # delete corresponding embedding
+        doclist = chroma_client.get(where={'docfileid': fileid})
+        if len(doclist['ids']) > 0:
+            chroma_client.delete(ids = doclist['ids'])
+            chroma_client.persist()
+        state['database']['parse_message'] = '+ Successfully deleted file from data store'
+    except Exception as e:
+        state['database']['parse_message'] = f"- {str(e)}\nPlease try again"    
 
 def get_llm_response(query: str):
     ret = chroma_client.as_retriever().get_relevant_documents(query)
     prompt = """
     Based on the following context, try to answer the question only using the context. If the answer is not provided in the context, do not try to guess, simply say that you do not know.
+    Also, it is VERY important that you do NOT use the word 'context' in your answer response.
 
     Examples:
 
--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import streamsync as ss
 import pandas as pd
 import datetime as dt
 from dependencies import TEMP_PDF_FILE_NAME
-from datastore import insert_pdf_to_store, get_llm_response
+from datastore import insert_pdf_to_store, get_llm_response, load_metadata_database, delete_pdf_from_store
 
 # This is a placeholder to get you started or refresh your memory.
 # Delete it or adapt it as necessary.
@@ -11,35 +11,39 @@ from datastore import insert_pdf_to_store, get_llm_response
 # Shows in the log when the app starts
 
 def get_chatbot_response(state):
+    state['chatbot']['response_visible'] = False
+    state['chatbot']['thinking_message'] = "% Thinking..."
     query = state['chatbot']['query']
-    state['chatbot']['response'] = get_llm_response(query)
-    state['chatbot']['response_visible'] = True
+    try:
+        state['chatbot']['response'] = get_llm_response(query)
+        state['chatbot']['thinking_message'] = '+ Here you go, please find the answer below.'
+        state['chatbot']['response_visible'] = True
+    except Exception as e:
+        state['chatbot']['thinking_message'] = f"- {str(e)}\nPlease try again"  
 
 def add_file_to_db(state):
-    input_file = state['database']['input_file']
-    if input_file is not None and len(input_file) > 0:
-        input_file = input_file[0]
-        fdata = input_file[0]
-
+    fdata = state['database']['input_file']
+    print(fdata)
+    if fdata is not None:
         with open(TEMP_PDF_FILE_NAME, 'wb') as f:
             f.write(fdata)
             f.close()
 
         # input the file into vector store 
-        insert_pdf_to_store(fdata)
+        insert_pdf_to_store(state['database']['input_file_name'], state)
+        state['database']['files'] = load_metadata_database()
+
 
 def file_change_name(state, payload):
     if payload is not None or len(payload) > 0:
         name = payload[0].get('name')
         state['database']['input_file_name'] = name
+        state['database']['input_file'] = payload[0].get('data')
 
 
 def db_selection_delete(state, context):
-    del state['database']['files'][context['itemid']]  # remove the item from DB
-
-    # remove embeddings and save into DB
-    pass
-
+    delete_pdf_from_store(context['itemId'], state)
+    state['database']['files'] = load_metadata_database()
 
 
 # Initialise the state
@@ -54,14 +58,15 @@ initial_state = ss.init_state({
         "primary_text": "#ffffff"
     },
     "database":  {
-        "files": {
-        },
+        "files": load_metadata_database(),
         "input_file": None,
-        'input_file_name': ""
+        'input_file_name': "",
+        "parse_message": ""
     },
     "chatbot": {
         "query": "",
         "response": "",
+        "thinking_message": "", 
         "response_visible": False
     }
 })
